22660811484240f8b9c506cf8ba75fc2728cb4e1
Suneet Nangia <suneetnangia@gmail.com>
2016-07-03 20:47:43 +0100

    Added machine key changes for web.config.

--- Microsoft.Sitecore/Microsoft.Sitecore.ResourceGroupDeployments/Scripts/SitecoreCDCMServerConfiguration.ps1
+++ Microsoft.Sitecore/Microsoft.Sitecore.ResourceGroupDeployments/Scripts/SitecoreCDCMServerConfiguration.ps1
@@ -1,4 +1,4 @@
-﻿param (
+param (
 [string]$remoteSitecoreFileUrl = "https://sitecore.blob.core.windows.net/setup/Sitecore8.1rev.51207.zip",
 [string]$remoteLicenseFileUrl = "https://sitecore.blob.core.windows.net/setup/license.xml",
 [string]$localZipFilePath = "d:\sitecoresetup.zip",
@@ -8,7 +8,11 @@
 [string]$masterDBConnectionString = "user id=sitecoreUser;password=;Data Source=SitecoreDBVM;Database=Sitecore.Master",
 [string]$webDBConnectionString = "user id=sitecoreUser;password=;Data Source=SitecoreDBVM;Database=Sitecore.Web",
 [string]$reportingDBConnectionString = "user id=sitecoreUser;password=;Data Source=SitecoreDBVM;Database=Sitecore.Reporting",
-[string]$sessionsDBConnectionString = "user id=sitecoreUser;password=;Data Source=SitecoreDBVM;Database=Sitecore.Sessions"
+[string]$sessionsDBConnectionString = "user id=sitecoreUser;password=;Data Source=SitecoreDBVM;Database=Sitecore.Sessions",
+[string]$macDecryption = "DES",
+[string]$macDecryptionKey = "DSFSFEWR3425452FDFS",
+[string]$macValidation = "HMACSHA128",
+[string]$macValidationKey = "DSFGDFDFSe3432523DSAFAFAS"
 )
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -174,6 +178,48 @@ Function Set-PrivateSessionSQLProvider{
 	}
 }
 
+Function Set-MACKey{
+	[CmdletBinding(SupportsShouldProcess=$True)]
+	Param(
+        [string]$fileName="D:\SitecoreSetup\Sitecore 8.1 rev. 151207\Website\web.config",
+		[string]$macDecryption="AES",
+		[string]$macDecryptionKey="470D1F481C6389C358F1E08C3DFADDDC4AF2A498597FF894EA40A6D488EDA000",
+		[string]$macValidation="HMACSHA256",
+		[string]$macValidationKey="D451149B2BAC52F48B2CB88160D4189EFEAE7FEC2A83818C19F0D521412C498856498DAC52DB58B28A86A31FE024A6EEC37C720AD261372C80059D09F06C3FD4"
+    )
+	
+	$config = [xml](Get-Content -LiteralPath $fileName) 
+	$element = $config.SelectSingleNode("configuration/system.web")
+    
+	if($element.SelectSingleNode("machineKey")) {
+
+		#Remove existing machine key if exist.
+        Write-Information "Machine key already exist, removing it..."    
+        $element.RemoveChild($element.SelectSingleNode("machineKey"))
+	}
+		$machineKeyElement = $config.CreateElement("machineKey")
+		$decryptionAttribute = $config.CreateAttribute("decryption")
+		$decryptionAttribute.Value = $macDecryption    
+		$decryptionKeyAttribute = $config.CreateAttribute("decryptionKey")
+		$decryptionKeyAttribute.Value = $macDecryptionKey
+		$validationAttribute = $config.CreateAttribute("validation")
+		$validationAttribute.Value = $macValidation
+		$validationKeyAttribute = $config.CreateAttribute("validationKey")
+		$validationKeyAttribute.Value = $macValidationKey
+       
+		$machineKeyElement.Attributes.Append($decryptionAttribute)
+		$machineKeyElement.Attributes.Append($decryptionKeyAttribute)
+		$machineKeyElement.Attributes.Append($validationAttribute)
+		$machineKeyElement.Attributes.Append($validationKeyAttribute)
+
+		$element.AppendChild($machineKeyElement)
+    
+		if($pscmdlet.ShouldProcess("$fileName","Add machine key to the web.config.")){
+			$config.Save($fileName)
+		}
+	
+}
+
 Function Set-SharedSessionSQLProvider{
 	[CmdletBinding(SupportsShouldProcess=$True)]
 	Param(
@@ -288,6 +334,9 @@ Set-CMSMode ("{0}\{1}" -f $setupLocalFolderPath, "\Website\App_Config\Include\Si
 #Configure SQL Private/Shared Session Provider
 Set-PrivateSessionSQLProvider ("{0}\{1}" -f $setupLocalFolderPath, "\Website\web.config") "sessions"
 Set-SharedSessionSQLProvider ("{0}\{1}" -f $setupLocalFolderPath, "\Website\App_Config\Include\Sitecore.Analytics.Tracking.config") "sessions"
+
+#Configure MAC key
+Set-MACKey ("{0}\{1}" -f $setupLocalFolderPath, "\Website\web.config") $macDecryption $macDecryptionKey $macValidation $macValidationKey
 
 #Copy to website directory
 Copy-Item ("{0}\{1}" -f $setupLocalFolderPath, "\Website\*") ("{0}" -f $websitePath) -Recurse -Force
